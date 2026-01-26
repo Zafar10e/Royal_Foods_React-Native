c8ca59fc008af52765577b9c258a32f4f37a4c23
Zafar Ali <zafar100e@gmail.com>
2026-01-26 19:14:45 +0500

    added appwrite variables to env file

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,6 +1,7 @@
 export const appwriteConfig = {
  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
- platform: 'com.Royal_Foods', projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
- databaseId: '6974c06e002be673ff4d',
- userCollectionId: 'user',
+ platform: 'com.Royal_Foods',
+ projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+ databaseId: process.env.APPWRITE_DATABASE_ID,
+ userCollectionId: process.env.APPWRITE_USERCOLLECTION_ID,
 }
